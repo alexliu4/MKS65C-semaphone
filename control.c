4ac72965cb47018633e1f5e109c3df47d4332d6b
alexliu4 <aliu9@stuy.edu>
2018-12-13 23:51:51 -0500

    fixing

--- control.c
+++ control.c
@@ -12,12 +12,12 @@
 
 #define KEY 0xDEADBEEF
 
-// union semun {
-//   int              val;    /* Value for SETVAL */
-//   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-//   unsigned short  *array;  /* Array for GETALL, SETALL */
-//   struct seminfo  *__buf;  /* Buffer for IPC_INFO */
-// };
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO */
+};
 
 void viewer(){
   int file;
